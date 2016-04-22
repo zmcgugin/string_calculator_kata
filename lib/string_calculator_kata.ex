855b76f6baddd18af99befc151f4c617a8287bc2
zmcgugin <fidelacchius@localhost.localdomain>
2016-04-22 16:35:16 -0400

    Adding exception

--- lib/string_calculator_kata.ex
+++ lib/string_calculator_kata.ex
@@ -1,16 +1,24 @@
 defmodule StringCalculatorKata do
   def calculate(numbers) do
-    numbers == "" && 0 || sum_list(convert_string_to_list_of_numbers(numbers))
+    default_empty_string_to_zero || sum_list(convert_string_to_list_of_numbers(numbers))
+  end
+
+  defp default_empty_string_to_zero(numbers) do
+    numbers == "" && 0 
   end
 
   defp convert_string_to_list_of_numbers(numbers) do
     numbers_list = Enum.map(convert_to_array(numbers), &(elem(Integer.parse(&1),0)))
+    check_for_negatives numbers_list
+    numbers_list
+  end
+
+  defp check_for_negatives(numbers_list) do
     negative_list = Enum.filter(numbers_list, &(&1 < 0))
     unless(length(negative_list) == 0) do
       error_numbers = Enum.join(negative_list, " ")
       raise "Negative numbers are not alowed: #{error_numbers}"
     end
-    numbers_list
   end
 
   defp convert_to_array(numbers) do
